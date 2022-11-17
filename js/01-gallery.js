e8033312208eac5fcffa144b8a74aacf9287ff83
Bohdan Polishchuk <polishuk.bog@gmail.com>
2022-11-18 01:51:19 +0200

    add start files

---
+++ js/01-gallery.js
@@ -0,0 +1,65 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+// Посилання на елементи в документі
+
+const ref = {
+  div: document.querySelector(".gallery"),
+};
+
+// Додаємо розмітку
+
+const markup = createGalleryItemMarkup(galleryItems);
+ref.div.insertAdjacentHTML("afterbegin", markup);
+
+// Додаємо слухач подій
+
+ref.div.addEventListener("click", onClickImage);
+
+// Функція створення розмітки з об'єкта
+
+function createGalleryItemMarkup(object) {
+  return object
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+<img class="gallery__image"
+src="${preview}"
+data-source="${original}"
+alt="${description}"/>
+</a>
+</div>`;
+    })
+    .join("");
+}
+
+// Функція кліку по зображенню
+
+function onClickImage(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`,
+    {
+      onShow: (instance) => {},
+
+      onClose: (instance) => {},
+    }
+  );
+
+  instance.show();
+}
+
+// Не можу зробити окрему функцію для закриття по клавіші escape, та передати її в OnShow
+//Хотів в onClose додати removeIventListener
+
+// function close() {
+// if (e.code === "Escape") {
+// instance.close();
+// }
+// }
